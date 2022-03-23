60030306f1a747485803b9f9aee945737263380b
Daniel S. Revne <danielsrevne@hotmail.no>
2022-03-23 10:12:38 +0100

    Some change. Not sure what

--- group16/__init__.py
+++ group16/__init__.py
@@ -4,3 +4,6 @@
 #p.prepare("stocks")
 #p.stock_stats("NFLX")
 
+
+# Improved sorting. In prepare. Going through line for line use binary search and insert.
+# Don't save stock name in every bite of data.
